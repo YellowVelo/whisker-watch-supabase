2e882d834e4681b39a793e91ff3aaee7076b1c54
Lynn <lynn@whiskerwatch.local>
2026-06-24 21:17:00 -0400

    Fix @ path alias in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -6,5 +7,10 @@ export default defineConfig({
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     react(),
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
