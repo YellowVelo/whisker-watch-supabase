7fedf9c9689058317d6e4576a5fc907f2b031b46
Lynn <lynn@whiskerwatch.local>
2026-07-04 13:08:13 -0400

    wrangler add

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 // https://vite.dev/config/
 export default defineConfig({
   logLevel: 'error', // Suppress warnings, only show errors
-  plugins: [
-    react(),
-  ],
+  plugins: [react(), cloudflare()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
